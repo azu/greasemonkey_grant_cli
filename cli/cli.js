c76d606983e573a0e0f5e31dc8687fc66e9980a4
azu <azuciao@gmail.com>
2014-06-22 15:52:55 +0900

    feat(cli): support command line

---
+++ cli/cli.js
@@ -0,0 +1,16 @@
+"use strict";
+var fs = require("fs");
+var path = require("path");
+var files = process.argv.slice(2);
+if (files.length === 0) {
+    throw new Error("usage: greasemonkey-grant file.js");
+}
+
+files.forEach(function (filePath) {
+    var code = fs.readFileSync(path.resolve(process.cwd, filePath), "utf-8");
+    var usedAPIList = require("../lib/listing-api")(code);
+    var output = require("../lib/generate-grant")(usedAPIList);
+    if (output != null) {
+        process.stdout.write(output);
+    }
+});
---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    generateGrant: require("./lib/generate-grant"),
+    listingAPI: require("./lib/listing-api"),
+    GreasemonkeyAPIList: require("./lib/Greasemonkey-API-list")
+};
